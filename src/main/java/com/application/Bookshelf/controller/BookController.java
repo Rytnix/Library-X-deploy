c0cb46edcdda760158442bb98c5e943cf31f3375
Utkarsh Yadav <utkarshrock1510@gmail.com>
2023-02-13 01:50:24 +0530

    fix

--- src/main/java/com/application/Bookshelf/controller/BookController.java
+++ src/main/java/com/application/Bookshelf/controller/BookController.java
@@ -12,10 +12,12 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/lm")
 public class BookController {
 
     @Autowired
